7d84f845496284c7964d8fa25308c215967f5660
sunwxg <sun.wxg@gmail.com>
2015-11-01 14:46:32 +0800

    add package examples

--- example_test.go
+++ example_test.go
@@ -1,6 +1,7 @@
 package goshark_test
 
 import (
+	"fmt"
 	"goshark"
 	"log"
 )
@@ -11,4 +12,31 @@ func ExampleDecoder_DecodeStart() {
 		log.Fatalf("Decode start fail: %s", err)
 	}
 	defer d.DecodeEnd()
+}
+
+func Example() {
+
+	file := "2.pcap"
+	d := goshark.NewDecoder()
+	if err := d.DecodeStart(file); err != nil {
+		log.Println("Decode start fail:", err)
+		return
+	}
+	defer d.DecodeEnd()
+
+	f, err := d.NextPacket()
+	if err != nil {
+		log.Println("Get packet fail:", err)
+		return
+	}
+
+	key := "igmp.maddr"
+	value, ok := f.Iskey(key)
+	if ok {
+		fmt.Printf("key: %s\nvalue: %s\n", key, value)
+	}
+
+	//Output:
+	//key: igmp.maddr
+	//value: 224.0.0.251
 }
